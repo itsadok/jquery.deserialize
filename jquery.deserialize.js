ee2fe1eb21e2d2335e37ca76b9ac60b73609f046
Israel Tsadok <itsadok@gmail.com>
2011-01-13 14:07:55 +0200

    use global jQuery instead of $

--- jquery.deserialize.js
+++ jquery.deserialize.js
@@ -7,16 +7,18 @@
  * $("form").deserialize(string);
  *
 **/
-$.fn.deserialize = function(s) {
-  var data = {};
-  var parts = s.split("&");
-  for (var i = 0; i < parts.length; i++) {
-    var pair = decodeURIComponent(parts[i]).split("=");
-    data[pair[0]] = pair[1];
-    $("[type!=radio][name='" + pair[0] + "']", this).val(pair[1]);
-    $("[type=radio][name='" + pair[0] + "'][value='" + pair[1] + "']", this).attr("checked", true);
-  }
-  $("input[type=checkbox]", this).each(function() {
-    $(this).attr("checked", $(this).attr("name") in data);
-  });
-};
+(function($) {
+    $.fn.deserialize = function(s) {
+      var data = {};
+      var parts = s.split("&");
+      for (var i = 0; i < parts.length; i++) {
+        var pair = decodeURIComponent(parts[i]).split("=");
+        data[pair[0]] = pair[1];
+        $("[type!=radio][name='" + pair[0] + "']", this).val(pair[1]);
+        $("[type=radio][name='" + pair[0] + "'][value='" + pair[1] + "']", this).attr("checked", true);
+      }
+      $("input[type=checkbox]", this).each(function() {
+        $(this).attr("checked", $(this).attr("name") in data);
+      });
+    };
+})(jQuery);
